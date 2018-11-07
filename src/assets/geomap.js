756997a34cf2de54ab2b0a782f20bbe5949fc6dd
Jan Ooms <flexyjean@gmail.com>
2018-11-07 21:09:38 +0100

    Geomap added for testing

---
+++ src/assets/geomap.js
@@ -0,0 +1,151 @@
+export default {
+  "type": "FeatureCollection",
+  "name": "1811061806",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [{
+      "type": "Feature",
+      "properties": {
+        "name": "A1",
+        "number": "1",
+        "fillColor": "#339933",
+        "fillOpacity": "1",
+        "stroke": 0
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [
+            [
+              [152, -152],
+              [2293, -152],
+              [1679, -1386],
+              [154, -1765],
+              [152, -152]
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "A4",
+        "fillColor": "#339933",
+        "fillOpacity": "1",
+        "stroke": false
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [
+            [
+              [7545, -150],
+              [8430, -150],
+              [8428, -2196],
+              [6646, -2209],
+              [7545, -150]
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "K4",
+        "fillColor": "#339933",
+        "fillOpacity": "1",
+        "stroke": false
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [
+            [
+              [5970, -7809],
+              [8428, -7977],
+              [8431, -9351],
+              [5933, -9349],
+              [5970, -7809]
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "K2",
+        "fillColor": "#339933",
+        "fillOpacity": "1",
+        "stroke": false
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [
+            [
+              [150, -8145],
+              [2372, -7753],
+              [2909, -7737],
+              [2968, -9353],
+              [150, -9351],
+              [150, -8145]
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "WWA1A2",
+        "fillColor": "#ffffff",
+        "fillOpacity": "1",
+        "stroke": false
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [
+            [
+              [1683, -1380],
+              [2294, -151],
+              [2464, -151],
+              [1832, -1412],
+              [1758, -1365],
+              [1683, -1380]
+            ]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "WWA2A3",
+        "fillColor": "#ffffff"
+      },
+      "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+          [
+            [
+              [4005, -149],
+              [4156, -147],
+              [4187, -1557],
+              [4129, -1525],
+              [4038, -1561],
+              [4005, -149]
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+};
