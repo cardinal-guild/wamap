8c808e8d0d657a655f4d82fc68fb4fb4b0cb3a60
Jan Ooms <flexyjean@gmail.com>
2018-11-25 13:37:00 +0100

    Babel building fix with old libraries

--- components/MapIslandCircles.vue
+++ components/MapIslandCircles.vue
@@ -74,10 +74,6 @@ export default {
         if (properties.type) {
           classNames.push(properties.type);
         }
-
-        if (properties.databanks) {
-          classNames.push('databanks-' + properties.databanks);
-        }
         if (leaflet) {
           return leaflet.divIcon({
             properties: properties,
@@ -86,7 +82,14 @@ export default {
               classNames.join(' ') +
               '"><img class="island-divicon-img" src="' +
               properties.imageIcon +
-              '" /><div class="island-divicon-type"></div><div class="island-image-divicon-revivor"></div></div>',
+              '" />' +
+              '<div class="island-divicon-type"></div>' +
+              '<div class="island-divicon-knowledge">' +
+              properties.databanks +
+              '</div>' +
+              '<div class="island-divicon-revival"></div>' +
+              '<div class="island-divicon-turrets"></div>' +
+              '</div>',
             iconSize: [100, 100],
             className: classNames.join(' ')
           });
@@ -112,16 +115,27 @@ export default {
     }
   }
   &.saborian {
+    .island-divicon-turrets:before,
+    .island-divicon-revival:before {
+      border-radius: 0;
+      transform: rotateZ(45deg);
+      width: 24px;
+      height: 24px;
+      top: 2px;
+      left: 2px;
+    }
+    .island-divicon-revival:after {
+      top: 2px;
+    }
     .island-divicon-type {
+      bottom: -14px;
       border-radius: 0;
       background: none;
       &:before {
-        content: ' ';
-        background: #ccc;
+        background: rgb(240, 240, 255);
+        border-radius: 0;
         width: 90%;
         height: 90%;
-        display: block;
-        position: absolute;
         top: -2.5%;
         left: 2.5%;
         transform: rotateZ(45deg);
@@ -132,19 +146,114 @@ export default {
     }
   }
   &.revival {
+    .island-divicon-revival {
+      display: block;
+    }
+  }
+
+  &.turrets {
+    .island-divicon-turrets {
+      display: block;
+    }
+  }
+  &-knowledge {
+    position: absolute;
+    display: block;
+    width: 36px;
+    height: 36px;
+    top: -15px;
+    left: 32px;
+    background-image: url('/assets/knowledge_icon.png');
+    color: black;
+    text-align: center;
+    padding: 7px;
+    font-size: 1.2em;
+    font-weight: 600;
+    font-family: 'Noto Sans', sans-serif;
+  }
+  &-revival {
+    display: none;
+    width: 28px;
+    height: 28px;
+    position: absolute;
+    top: 36px;
+    right: -14px;
+    &:before {
+      content: ' ';
+      background: rgb(20, 160, 20);
+      box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.75);
+      border-radius: 100%;
+      width: 100%;
+      height: 100%;
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    &:after {
+      content: ' ';
+      background-image: url('/assets/revival_icon.png');
+      width: 20px;
+      height: 20px;
+      display: block;
+      position: absolute;
+      top: 4px;
+      left: 4px;
+    }
+  }
+  &-turrets {
+    display: none;
+    width: 28px;
+    height: 28px;
+    position: absolute;
+    top: 36px;
+    left: -14px;
+
+    &:before {
+      content: ' ';
+      background: rgb(160, 20, 20);
+      box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.75);
+      border-radius: 100%;
+      width: 100%;
+      height: 100%;
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    &:after {
+      content: ' ';
+      background-image: url('/assets/turret_icon.png');
+      width: 20px;
+      height: 20px;
+      display: block;
+      position: absolute;
+      top: 4px;
+      left: 4px;
+    }
   }
   &-type {
-    border-radius: 100%;
-    display: block;
-    background: #ccc;
+    &:before {
+      content: ' ';
+      background: rgb(255, 240, 220);
+      box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.75);
+      border-radius: 100%;
+      width: 100%;
+      height: 100%;
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
     width: 32px;
     height: 32px;
     position: absolute;
     bottom: -16px;
     left: 35px;
+    display: block;
     &:after {
       content: ' ';
-      background-size: cover;
       width: 24px;
       height: 24px;
       margin: 4px;
