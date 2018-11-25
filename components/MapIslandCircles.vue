e934a78f878d486c1fe089d85d74171391737718
Jan Ooms <flexyjean@gmail.com>
2018-11-25 12:09:04 +0100

    Pseudo element icon

--- components/MapIslandCircles.vue
+++ components/MapIslandCircles.vue
@@ -61,7 +61,7 @@ export default {
     },
     getIconImage: properties => {
       if (properties.imageIcon) {
-        let classNames = ['island-image-icon'];
+        let classNames = ['island-divicon'];
         if (properties.revivalChambers) {
           classNames.push('revival');
         }
@@ -79,9 +79,14 @@ export default {
           classNames.push('databanks-' + properties.databanks);
         }
         if (leaflet) {
-          return leaflet.icon({
+          return leaflet.divIcon({
             properties: properties,
-            iconUrl: properties.imageIcon,
+            html:
+              '<div class="' +
+              classNames.join(' ') +
+              '"><img class="island-divicon-img" src="' +
+              properties.imageIcon +
+              '" /><div class="island-divicon-type"></div><div class="island-image-divicon-revivor"></div></div>',
             iconSize: [100, 100],
             className: classNames.join(' ')
           });
@@ -93,19 +98,60 @@ export default {
 </script>
 
 <style lang="scss">
-.island-image-icon {
-  border-radius: 100%;
-  border: 5px solid #ccc;
-  box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.75);
+.island-divicon {
+  &-img {
+    width: 100px;
+    height: 100px;
+    border-radius: 100%;
+    border: 5px solid #ccc;
+    box-shadow: 2px 2px 10px 0px rgba(0, 0, 0, 0.75);
+  }
+  &.kioki {
+    .island-divicon-type:after {
+      background-image: url('/assets/kioki_icon.png');
+    }
+  }
+  &.saborian {
+    .island-divicon-type {
+      border-radius: 0;
+      background: none;
+      &:before {
+        content: ' ';
+        background: #ccc;
+        width: 90%;
+        height: 90%;
+        display: block;
+        position: absolute;
+        top: -2.5%;
+        left: 2.5%;
+        transform: rotateZ(45deg);
+      }
+      &:after {
+        background-image: url('/assets/saborian_icon.png');
+      }
+    }
+  }
   &.revival {
-    &:before {
-      z-index: 2;
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      content: 'test';
-      top: 0;
+  }
+  &-type {
+    border-radius: 100%;
+    display: block;
+    background: #ccc;
+    width: 32px;
+    height: 32px;
+    position: absolute;
+    bottom: -16px;
+    left: 35px;
+    &:after {
+      content: ' ';
+      background-size: cover;
+      width: 24px;
+      height: 24px;
+      margin: 4px;
       display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
     }
   }
 }
