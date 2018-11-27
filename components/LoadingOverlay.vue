84de6641700e80f1982ead0385810a6bff1fc53f
Jan Ooms <flexyjean@gmail.com>
2018-11-27 17:30:13 +0100

    Loading overlay improved

--- components/LoadingOverlay.vue
+++ components/LoadingOverlay.vue
@@ -1,18 +1,16 @@
 <template>
-    <div class="fullscreen-loader text-xs-center" v-if="loading || boundariesLoading || islandsLoading">
-        <v-container bg fill-height grid-list-md text-xs-center>
-        <v-layout row wrap align-center>
-          <v-flex>
-             <v-progress-circular
-            indeterminate
-            color="primary"
-            aria-setsize="20"
-        ></v-progress-circular>
-        Loading map data ... 
-          </v-flex>
-        </v-layout> 
-      </v-container>
-    </div>
+  <div
+    class="fullscreen-loader text-xs-center"
+    v-if="loading || boundariesLoading || islandsLoading"
+  >
+    <v-container bg fill-height grid-list-md text-xs-center>
+      <v-layout row wrap align-center>
+        <v-flex>
+          <v-progress-circular indeterminate color="primary" aria-setsize="20"></v-progress-circular>Loading map data ...
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -33,13 +31,15 @@ export default {
 
 <style lang="scss" scoped>
 .fullscreen-loader {
-  background-color: rgba(255, 255, 255, 0.9);
+  background-color: rgba(0, 0, 0, 0.9);
   position: absolute;
   top: 0;
   bottom: 0;
   right: 0;
   left: 0;
   font-size: 1.2rem;
+  z-index: 2000;
+  pointer-events: all;
   .v-progress-circular {
     margin-right: 20px;
   }
