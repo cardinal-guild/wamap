313fdbbb12dd23b94e47832a1a2e5456b12c38b6
Jan Ooms <flexyjean@gmail.com>
2018-12-27 14:24:07 +0100

    Overheat calculation fully implemented

--- pages/overheat/index.vue
+++ pages/overheat/index.vue
@@ -1,58 +1,65 @@
 <template>
-  <div class="configuration-page">
-    <v-container>
-      <v-layout column justify-center align-center>
-        <v-alert
-          :value="true"
-          type="error"
-        >We are currently implementing the system for overheat calculation, not finished yet!</v-alert>
-        <v-card color="#4f4141">
-          <v-card-actions>
-            <table>
-              <tr>
-                <th colspan="2" style="align-text:center">Please fill in your engine properties</th>
-              </tr>
-              <tr>
-                <td>
-                  <v-form ref="overheatCalculationForm" lazy-validation>
-                    <v-list>
-                      <v-list-tile>
-                        <v-list-tile-title>Cooling Factor</v-list-tile-title>
-                        <v-list-tile-action>
-                          <v-input-number
-                            label="Your engine Cooling Factor"
-                            v-model="coolingFactor"
-                            :outline="false"
-                            :min="0"
-                            :max="100"
-                          ></v-input-number>
-                        </v-list-tile-action>
-                      </v-list-tile>
-                      <v-list-tile>
-                        <v-list-tile-title>Power</v-list-tile-title>
-                        <v-list-tile-action>
-                          <v-input-number
-                            label="Your engine Power"
-                            v-model="power"
-                            :outline="false"
-                            :min="0"
-                            :max="100"
-                          ></v-input-number>
-                        </v-list-tile-action>
-                      </v-list-tile>
-                      <v-list-tile>
-                        <v-list-tile-title>Overheat Limit</v-list-tile-title>
-                        <v-list-tile-action>
-                          <v-input-number
-                            label="Your engine Overheat Limit"
-                            v-model="overheatLimit"
-                            :outline="false"
-                            :min="0"
-                            :max="100"
-                          ></v-input-number>
-                        </v-list-tile-action>
-                      </v-list-tile>
-                      <v-list-tile>
+  <div class="overheat-page">
+    <no-ssr>
+      <v-container>
+        <v-layout column justify-center align-center>
+          <v-alert :value="true" type="info" width="800">
+            This is very experimental, but based upon the post of
+            <a
+              href="https://www.worldsadrift.com/forums/topic/overheat-equation/"
+            >overheat equation</a> on the worlds adrift forums.
+            With the chart below you can pinpoint if you have good or bad engines and know the overheat factor you need.
+          </v-alert>
+          <v-card color="#4f4141">
+            <v-card-actions>
+              <table>
+                <tr>
+                  <th colspan="2" style="align-text:center">Please fill in your engine properties</th>
+                </tr>
+                <tr>
+                  <td>
+                    <v-form ref="overheatCalculationForm" lazy-validation>
+                      <v-list>
+                        <v-list-tile>
+                          <v-list-tile-title>Cooling Factor</v-list-tile-title>
+                          <v-list-tile-action>
+                            <v-input-number
+                              label="Your engine Cooling Factor"
+                              v-model="coolingFactor"
+                              :outline="false"
+                              :min="0"
+                              :max="100"
+                              @input="calculateOverheat"
+                            ></v-input-number>
+                          </v-list-tile-action>
+                        </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-title>Power</v-list-tile-title>
+                          <v-list-tile-action>
+                            <v-input-number
+                              label="Your engine Power"
+                              v-model="power"
+                              :outline="false"
+                              :min="0"
+                              :max="100"
+                              @input="calculateOverheat"
+                            ></v-input-number>
+                          </v-list-tile-action>
+                        </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-title>Overheat Limit</v-list-tile-title>
+                          <v-list-tile-action>
+                            <v-input-number
+                              label="Your engine Overheat Limit"
+                              v-model="overheatLimit"
+                              :outline="false"
+                              :min="0"
+                              :max="100"
+                              @input="calculateOverheat"
+                            ></v-input-number>
+                          </v-list-tile-action>
+                        </v-list-tile>
+                        <!-- <v-list-tile>
                         <v-list-tile-title>
                           Current ship weight
                           <small>(check shipyard)</small>
@@ -93,17 +100,34 @@
                             :max="10"
                           ></v-input-number>
                         </v-list-tile-action>
-                      </v-list-tile>
-                    </v-list>
-                    <v-btn block>Calculate overheat graph</v-btn>
-                  </v-form>
-                </td>
-              </tr>
-            </table>
-          </v-card-actions>
-        </v-card>
-        <no-ssr>
-          <graph-line-timerange
+                        </v-list-tile>-->
+                      </v-list>
+                    </v-form>
+                  </td>
+                </tr>
+              </table>
+            </v-card-actions>
+          </v-card>
+          <br>
+
+          <h3
+            class="subheading"
+          >Your engine will not overheat when throttled below or equal to {{ maxThrottlePercentage }} %</h3>
+          <br>
+          <graph-bar
+            :width="800"
+            :height="400"
+            :axis-min="axisMin"
+            :axis-max="axisMax"
+            :labels="labels"
+            :values="values"
+            :colors="[ '#3333FF', '#FF3333' ]"
+            v-if="showGraph"
+          >
+            <note :text="'Overheat throttle chart (Higher is worse)'"></note>
+            <tooltip :names="['Freezing factor','Overheating factor']" :position="'left'"></tooltip>
+          </graph-bar>
+          <!-- <graph-line-timerange
             :width="600"
             :height="200"
             :axis-min="0"
@@ -144,10 +168,10 @@
           >
             <note :text="'Travel distance before overheating'"></note>
             <guideline :tooltip-x="true" :tooltip-y="true"></guideline>
-          </graph-line-timerange>
-        </no-ssr>
-      </v-layout>
-    </v-container>
+          </graph-line-timerange>-->
+        </v-layout>
+      </v-container>
+    </no-ssr>
   </div>
 </template>
 <script>
@@ -156,26 +180,55 @@ export default {
   mounted () {
     this.$store.commit('setShowMapControls', false);
     this.$store.commit('setPageTitle', this.title);
+    this.calculateOverheat();
+  },
+  methods: {
+    calculateOverheat () {
+      let setLabels = [];
+      let setFreezings = [];
+      let setOverheats = [];
+      let coolingFactorPercentage = this.coolingFactor / 100;
+      this.maxThrottlePercentage = parseInt(
+        100 -
+          (50 + (1 - coolingFactorPercentage) * this.power - this.overheatLimit)
+      );
+      let i;
+      for (i = 0; i < 20; i++) {
+        let calculatedOverheat =
+          (1 - coolingFactorPercentage) * this.power * (i * 0.05) -
+          this.overheatLimit / 2;
+        setLabels.push(i * 5 + '%');
+        if (calculatedOverheat < 0) {
+          setFreezings.push(parseFloat(calculatedOverheat).toFixed(2));
+          setOverheats.push(0);
+        } else {
+          setFreezings.push(0);
+          setOverheats.push(parseFloat(calculatedOverheat).toFixed(2));
+        }
+      }
+      this.axisMin = setFreezings[0];
+      this.axisMax = setOverheats[19];
+      this.labels = setLabels;
+      this.values = [setFreezings, setOverheats];
+      this.showGraph = true;
+    }
   },
   head () {
     return { title: 'Cardinal Guild - ' + this.title };
   },
   data () {
     return {
+      showGraph: false,
       title: 'Overheat calculator',
-      coolingFactor: 0,
-      overheatLimit: 0,
-      power: 0,
+      coolingFactor: 38,
+      power: 67,
+      overheatLimit: 64,
+      maxThrottlePercentage: 0,
+      axisMin: -50,
+      axisMax: 50,
       shipWeight: 0,
-      labels: [new Date('2018-1-1 00:00:00'), new Date('2018-1-1 00:05:00')],
-      values: [
-        [new Date('2018-1-1 00:00:00'), 55],
-        [new Date('2018-1-1 00:01:00'), 30],
-        [new Date('2018-1-1 00:02:00'), 25],
-        [new Date('2018-1-1 00:03:00'), 40],
-        [new Date('2018-1-1 00:04:00'), 45],
-        [new Date('2018-1-1 00:05:00'), 70]
-      ]
+      labels: null,
+      values: null
     };
   }
 };
